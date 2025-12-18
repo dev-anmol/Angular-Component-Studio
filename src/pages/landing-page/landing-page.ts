6ab20f0318cf6a24f2886d4eaec5dd669d34d874
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-12-18 23:57:43 +0530

    added header

---
+++ src/pages/landing-page/landing-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {Header} from '../../components/header/header';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [
+    Header
+  ],
+  templateUrl: './landing-page.html',
+})
+export class LandingPage {
+
+}
