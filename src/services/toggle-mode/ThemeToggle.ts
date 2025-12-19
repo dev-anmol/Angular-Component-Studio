2995bc484834be1b9aca6020add5defe05c214c8
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-12-20 01:33:22 +0530

    added toggle theme service

---
+++ src/services/toggle-mode/ThemeToggle.ts
@@ -0,0 +1,17 @@
+import {Injectable, signal, WritableSignal} from '@angular/core';
+import {Theme} from '../../types/theme';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeToggle {
+
+  currentMode = signal<Theme>('night');
+
+
+  toggle() {
+    this.currentMode.update(theme =>
+      theme === 'day' ? 'night' : 'day'
+    )
+  }
+}
