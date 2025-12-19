55a82da02e38fa1cd0accc4e7ab4368d63873041
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-12-20 01:19:20 +0530

    landing page updated and toggle service

---
+++ src/components/hero/hero.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {ArrowRight, LucideAngularModule} from 'lucide-angular';
+
+@Component({
+  selector: 'app-hero',
+  imports: [
+    LucideAngularModule
+  ],
+  templateUrl: './hero.html',
+})
+export class Hero {
+
+  protected readonly ArrowRight = ArrowRight;
+}
--- src/pages/landing-page/landing-page.ts
+++ src/pages/landing-page/landing-page.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Header} from '../../components/header/header';
+import {Hero} from '../../components/hero/hero'
 
 @Component({
   selector: 'app-landing-page',
   imports: [
-    Header
+    Header, Hero
   ],
   templateUrl: './landing-page.html',
 })
