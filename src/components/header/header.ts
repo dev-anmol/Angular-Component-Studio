406ce6114d4e3b2bad7562d152e542e57c4ce61b
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-12-19 22:50:18 +0530

    frontend changes

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {LucideAngularModule, ArrowRight, LucideIconData, MenuIcon} from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './header.html',
 })
 export class Header {
-
+  readonly ArrowRight: LucideIconData = ArrowRight;
+  readonly MenuIcon: LucideIconData = MenuIcon;
 }
