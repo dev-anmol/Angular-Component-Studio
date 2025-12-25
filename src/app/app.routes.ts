2d728f1d24bd32544a9a6e07707c953051572fed
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-12-25 19:02:18 +0530

    updated routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,45 @@
 import {Routes} from '@angular/router';
 import {AppShell} from '../layout/app-shell/app-shell';
 
-export const routes: Routes = [{
-  path: '',
-  loadComponent: () => import('../pages/landing-page/landing-page').then(m => m.LandingPage),
-}, {
-  path: '',
-  component: AppShell,
-  children: [
-    {
-      path: 'docs',
-      children: [
-        {
-          // This route contains the introduction regarding the application i.e the problems, solutions and features
-          path: '',
-          loadComponent: () => import('../pages/introduction/introduction')
-            .then(m => m.Introduction)
-        }, {
-          // This path contains all the components and users can navigate to individual components by selecting
-          path: 'components',
-          loadComponent: () => import('../pages/components/component-page/component-page')
-            .then(m => m.ComponentPage)
-        },
-        {
-          // This path contains the details regarding the individual component.
-          path: 'components/:slug',
-          loadComponent: () =>
-            import('../pages/components/component-detail/component-detail').then(m => m.ComponentDetail),
-        }
-      ]
-    },
-  ]
-}];
+export const routes: Routes = [
 
+  // Landing page (NO shell)
+  {
+    path: '',
+    loadComponent: () =>
+      import('../pages/landing-page/landing-page')
+        .then(m => m.LandingPage),
+  },
+
+  // Docs area (WITH shell)
+  {
+    path: 'docs',
+    component: AppShell,
+    children: [
+
+      // Introduction
+      {
+        path: '',
+        loadComponent: () =>
+          import('../pages/introduction/introduction')
+            .then(m => m.Introduction),
+      },
+
+      // Components overview
+      {
+        path: 'components',
+        loadComponent: () =>
+          import('../pages/components/component-page/component-page')
+            .then(m => m.ComponentPage),
+      },
+
+      // Component detail
+      {
+        path: 'components/:slug',
+        loadComponent: () =>
+          import('../pages/components/component-detail/component-detail')
+            .then(m => m.ComponentDetail),
+      },
+    ],
+  },
+];
