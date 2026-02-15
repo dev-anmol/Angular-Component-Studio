c68fb2bb9b8a053774e7eae4d2e65811054ad6dd
Anmol Thakur <anmolll.thakurrr@gmail.com>
2026-02-15 21:47:19 +0530

    fixed issues

--- src/pages/introduction/introduction.ts
+++ src/pages/introduction/introduction.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CodeBlock } from '../../shared/code-block/code-block';
 
 @Component({
   selector: 'app-introduction',
-  imports: [RouterLink],
+  imports: [RouterLink, CodeBlock],
   templateUrl: './introduction.html',
 })
 export class Introduction {
-
+  cliInstall = `npm install -g @angular/cli@21`;
+  newProject = `ng new my-app --ssr`;
 }
